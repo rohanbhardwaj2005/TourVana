d914b07c90366fa0fc6f4e4f635bb85f7be4916d
Rohan Bhardwaj <113487993+rohanbhardwaj2005@users.noreply.github.com>
2024-07-09 06:26:46 +0000

    Minor updates

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -3,9 +3,7 @@ import { showAlert } from "./alerts";
 
 export const bookTour = async (tourId) => {
   try {
-    const session = await axios(
-      `https://87772f31-5160-484a-9683-9a8a95fd69ff-00-1w0f1vzamlbqn.riker.replit.dev/api/v1/bookings/checkout-session/${tourId}`,
-    );
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     window.location.assign(session.data.session.url);
   } catch (err) {
     // console.log(err.stack);
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -7,8 +7,8 @@ export const updateSettings = async (data, type) => {
   try {
     const url =
       type === "password"
-        ? "https://87772f31-5160-484a-9683-9a8a95fd69ff-00-1w0f1vzamlbqn.riker.replit.dev/api/v1/users/updateMyPassword"
-        : "https://87772f31-5160-484a-9683-9a8a95fd69ff-00-1w0f1vzamlbqn.riker.replit.dev/api/v1/users/updateMe";
+        ? "/api/v1/users/updateMyPassword"
+        : "/api/v1/users/updateMe";
 
     const res = await axios({
       method: "PATCH",
